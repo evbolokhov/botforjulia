214ccd463805cf54187a401eee258ca54e3f4832
evbolokhov <evbolohov@yandex.ru>
2023-06-18 16:24:15 +0900

    add многопоточность, 10 криптопар

--- main.py
+++ main.py
@@ -2,73 +2,93 @@ import api
 from keys import *
 import numpy
 import time
-import matplotlib.pyplot as plt
+import threading
+
+
+class SymbolThread(threading.Thread):
+    def __init__(self, symbol):
+        super(SymbolThread, self).__init__()
+        self.symbol = symbol
+        self.client = api.Binance_API(api_key=api_key, secret_key=secret_key)
+        self.stop_event = threading.Event()
+        self.wait_time = None
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        # Ожидание начала новой минутной свечи
+        self.wait_time = round(10 - time.time() % 10 + 1)
+        time.sleep(self.wait_time)
+        while not self.stop_event.is_set():
+            klines = self.client.get_candles(symbol=self.symbol, interval='1m')
+            last_candle = klines[-1]
+            if time.time() < last_candle[6]:
+                klines.pop()
+
+            numpy_klines = numpy.array(klines)
+            close_price = numpy_klines[:, 4]
+            close_price = close_price.astype(float)
+            print("{} close_price: {}".format(self.symbol, close_price[-1]))
+            ema_short = api.ema(close_price, 2)
+            ema_long = api.ema(close_price, 5)
+            rsi = (api.rsi(close_price, 14))
+            print("{} rsi: {}".format(self.symbol, rsi[-1]))
+            bol = (api.bollinger_breakout_strategy(close_price, 20, 2))
+            positions, upper_band, lower_band = bol
+            print("{} Positions: {}".format(self.symbol, positions[-1]))
+            print("{} Upper Band: {}".format(self.symbol, upper_band[-1]))
+            print("{} Lower Band: {}".format(self.symbol, lower_band[-1]))
+            short_value = ema_short[-1]
+            prev_short_value = ema_short[-2]
+            long_value = ema_long[-1]
+            prev_long_value = ema_long[-2]
+
+            # Long
+            if short_value > long_value and prev_short_value < prev_long_value and rsi[-1] > 70 and positions[-1] == 1:
+                print("{} Long".format(self.symbol))
+                qnt = 5  # значение количества, которое вы хотите использовать для торговли
+                long = self.client.create_market_order(symbol=self.symbol, side='BUY', qnt=qnt)
+                print("{} Bought {} units of {}".format(self.symbol, qnt, self.symbol.split("USDT")[0]))
+            # Short
+            elif prev_short_value > prev_long_value and short_value < long_value and rsi[-1] < 30 and positions[
+                -1] == -1:
+                print("{} Short".format(self.symbol))
+                qnt = 5  # значение количества, которое вы хотите использовать для торговли
+                short = self.client.create_market_order(symbol=self.symbol, side='SELL', qnt=qnt)
+                print("{} Sold {} units of {}".format(self.symbol, qnt, self.symbol.split("USDT")[0]))
+            else:
+                print('{} No signal'.format(self.symbol))
+
+            # Добавляем ожидание до следующей минутной свечи
+            wait_time_next = round(10 - time.time() % 10 + 1)
+            print("До начала следующей минутной свечи: {} сек.".format(wait_time_next))
+            time.sleep(wait_time_next)
+
+        # Печатаем сообщение после того, как поток остановится
+        print("Поток {} остановлен".format(self.symbol))
+
+
+def start_threads():
+
+    symbols = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "XRPUSDT", "DOTUSDT", "BNBUSDT", "DOGEUSDT", "LTCUSDT", "LINKUSDT",
+               "BCHUSDT"]
+    threads = []
+
+    # Запускаем отдельный поток для каждой криптовалютной пары
+    for symbol in symbols:
+        thread = SymbolThread(symbol)
+        thread.start()
+        threads.append(thread)
+    wait_times = [thread.wait_time for thread in threads]
+    print("До начала новой минутной свечи: {} сек.".format(min(wait_times)))
+    # Ожидаем завершения всех потоков
+    for thread in threads:
+        thread.join()
+
+    # Печатаем сообщение после того, как все потоки завершили
+    print("Все потоки завершены")
+
 
 if __name__ == '__main__':
-    client = api.Binance_API(api_key=api_key, secret_key=secret_key)
-
-    while True:
-        print(10 - time.time() % 10)
-        time.sleep(10 - time.time() % 10 + 1)
-        klines = client.get_candles(symbol="NEARUSDT", interval='1m')
-        last_candle = klines[-1]
-        # print(last_candle)
-        # print(time.time(), last_candle[6])
-        if time.time() < last_candle[6]:
-            klines.pop()
-
-        numpy_klines = numpy.array(klines)
-        # print("numpy_klines", numpy_klines)
-        close_price = numpy_klines[:, 4]
-        close_price = close_price.astype(float)
-        print("close_price:", close_price[-1])
-        ema_short = api.ema(close_price, 2)
-        ema_long = api.ema(close_price, 5)
-        rsi = (api.rsi(close_price, 14))
-        print("rsi:", rsi[-1])
-        bol = (api.bollinger_breakout_strategy(close_price, 20, 2))
-        positions, upper_band, lower_band = bol
-        print("Positions:", positions[-1])
-        print("Upper Band:", upper_band[-1])
-        print("Lower Band:", lower_band[-1])
-        # print(ema_short)
-        short_value = ema_short[-1]
-        prev_short_value = ema_short[-2]
-        # print(short_value)
-        # print(prev_short_value)
-        # print(ema_long)
-        long_value = ema_long[-1]
-        prev_long_value = ema_long[-2]
-
-        # Long
-        if short_value > long_value and prev_short_value < prev_long_value and rsi[-1] > 70 and positions[-1] == 1:
-            print("Long")
-            long = client.create_market_order(symbol='NEARUSDT', side='BUY', qnt=3)
-            print(long)
-        # Short
-        elif prev_short_value > prev_long_value and short_value < long_value and rsi[-1] < 30 and positions[-1] == -1:
-            print("Short")
-            short = client.create_market_order(symbol='NEARUSDT', side='SELL', qnt=3)
-            print(short)
-        else:
-            print('No signal')
-        # # генерируем данные
-        # positions, upper_band, lower_band = api.bollinger_breakout_strategy(close_price, 20, 2)
-        #
-        # # рисуем график цен закрытия
-        # plt.plot(close_price)
-        #
-        # # рисуем график верхней полосы Боллинджера
-        # plt.plot(upper_band)
-        #
-        # # рисуем график нижней полосы Боллинджера
-        # plt.plot(lower_band)
-        #
-        # # добавляем легенду
-        # plt.legend(["Close Price", "Upper Band", "Lower Band"])
-        #
-        # # добавляем заголовок
-        # plt.title("Bollinger Bands")
-        #
-        # # показываем график
-        # plt.show()
+    start_threads()
